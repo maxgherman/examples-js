2f1b07a66db13f3d9e84f1ace29b4e75c85602ce
Max Gherman <german.maxim@gmail.com>
2019-06-17 16:37:14 +1000

    Added sources

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { createExampleElement } = require('./src/scaffold')
 const examples = require('./src/all')
 
-const root = document.getElementById('app');
+const root = document.getElementById('root');
 
 examples.reduce((acc, curr, index) => {
     acc.appendChild(createExampleElement(index, curr.name, curr.value))
